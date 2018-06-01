834ae6344cb1f1acf3397c27b03cfc5ee3201868
fullhdpixel <tsmudde@hotmail.com>
2018-06-01 23:36:51 +0200

    first release

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import {render} from 'react-dom'
-
-import FullscreenControl from '../../src'
+import {Map, TileLayer} from 'react-leaflet'
 
 import './leaflet/leaflet.js'
 import './leaflet/leaflet.css'
 
-import 'leaflet-fullscreen'
-
-import {Map, TileLayer} from 'react-leaflet'
+import '../../src/lib/leaflet.fullscreen.css'
+import '../../src/lib/leaflet.fullscreen.min.js'
 
 const App = () => (
-  <Map 
-  fullscreenControl
-  style={{
+  <Map
+    fullscreenControl
+    style={{
     height: '100vh'
-  }} zoom={13} center={[51.505, -0.09]}>
-    {/* <FullscreenControl/> */}
+  }}
+    zoom={13}
+    center={[51.505, -0.09]}>
 
     <TileLayer
       attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-import FullscreenControl from './FullscreenControl'
-
-export default FullscreenControl
+import './lib/leaflet.fullscreen.min.js'
+import './lib/leaflet.fullscreen.css'
